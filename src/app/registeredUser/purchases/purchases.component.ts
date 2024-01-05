2506d39b1355a6218709dc29c6f921bacfc8105e
Moya Ramiro <moya04.ramiro@gmail.com>
2024-01-04 23:22:39 -0300

    iniciando historial, mis compras

--- src/app/registeredUser/purchases/purchases.component.ts
+++ src/app/registeredUser/purchases/purchases.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { sideNavComponent } from '../sideNav/sideNav.component';
+import { soundPacksData } from '../sound-packs/soundPacks-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-purchases',
   standalone: true,
-  imports: [sideNavComponent],
+  imports: [sideNavComponent, CommonModule],
   templateUrl: './purchases.component.html',
   styleUrl: './purchases.component.css'
 })
 export class PurchasesComponent {
 
+  soundPacks = soundPacksData;
 }
