6b570ad138c4a020e36b960993ef23288ce9e410
Moya Ramiro <moya04.ramiro@gmail.com>
2024-01-08 23:53:09 -0300

    agregando las rutas a las funcionalidades de los botones

--- src/app/registeredUser/sideNav/sideNav.component.ts
+++ src/app/registeredUser/sideNav/sideNav.component.ts
@@ -1,28 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ChangeDetectorRef, ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-sideNav',
   standalone: true,
   imports: [CommonModule, RouterLink],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './sideNav.component.html',
   styleUrl: './sideNav.component.css'
 })
 export class sideNavComponent {
 
-  constructor(private auth : AuthService) {}
+  constructor(private auth : AuthService, private router : Router, private cdr: ChangeDetectorRef) {}
 
   getUserInfo() {
     return this.auth.user$;
   }
 
-  currentSection: string = 'feed';
+  currentSection: string = '';
   activeButton: string = 'feed';
 
   changeSection(section: string) {
     this.currentSection = section;
+    this.router.navigateByUrl(`/${section}`)
+    this.cdr.detectChanges();
 
   }
 
