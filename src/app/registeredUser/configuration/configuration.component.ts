475244d4afea477cc08151fdba85b5b3bf575d29
Moya Ramiro <moya04.ramiro@gmail.com>
2024-04-19 16:54:50 -0300

    correcciones visuales

--- src/app/registeredUser/configuration/configuration.component.ts
+++ src/app/registeredUser/configuration/configuration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { sideNavComponent } from '../sideNav/sideNav.component';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-configuration',
@@ -9,5 +10,9 @@ import { sideNavComponent } from '../sideNav/sideNav.component';
   styleUrl: './configuration.component.css'
 })
 export class ConfigurationComponent {
-
+  constructor (private auth : AuthService) {}
+  
+  getUserInfo (){
+    this.auth.user$;
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit', 
+  content: ['./src/app/**/*.{html,js,ts}'],
+  theme: {
+    extend: {}, 
+  },
+  plugins: [], 
+};
